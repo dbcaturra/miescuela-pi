8c85eae4e9eb165925eded13a2c8b8f8f74a646c
Ubuntu <dbcaturra@gmail.com>
2019-08-27 03:36:21 +0000

    Commit message

--- escuela-orquesta.sh
+++ escuela-orquesta.sh
@@ -8,15 +8,15 @@ fi
 if [ ! -d "docker-tao-web" ] ; then
     git clone https://github.com/kartoza/docker-tao-web.git
 fi
-if [ ! -d "docker-compose-moodle" ] ; then
-    git clone https://github.com/jobcespedes/docker-compose-moodle.git
+if [ ! -d "docker-moodle" ] ; then
+    https://github.com/jmhardison/docker-moodle.git
 fi
-cd gibbon-docker && docker-compose up -d
-echo "gibbonedu up..."
-cd .. && cd docker-compose-moodle
-docker-compose up  -d
-echo "moodle up..." && cd ..
-cd docker-tao-web
+#cd gibbon-docker && docker-compose up -d
+#echo "gibbonedu up..."
+#cd .. && cd docker-compose-moodle
+#docker-compose up  -d
+#echo "moodle up..." && cd ..
+#cd docker-tao-web
 #./build.sh
 ./run.sh
 echo "tao-web up..."
